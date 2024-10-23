d0805f55cbd59b7e8fe0ce4cce7c545ea1decc67
nicolas.patigny <nickim.patigny@gmail.com>
2024-10-23 18:47:31 +0200

    Added function playRound

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-function getComputerChoice(){
-   let choice = Math.floor(Math.random() * 3);
-   if (choice == 0){
-        return 'Rock'
-   }  else if(choice == 1){
-        return 'Paper'
-   } else if(choice == 2){
-        return 'Scissor'
-   }
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice == 0) {
+    return "Rock";
+  } else if (choice == 1) {
+    return "Paper";
+  } else if (choice == 2) {
+    return "Scissors";
+  }
 }
 
-function getPlayerChoice(){
-    let choice = prompt("Choose Rock, Paper or Scissors?");
-    if (choice.toLowerCase() == 'rock'){
-        return 'Rock'
-    } else if(choice.toLowerCase() == 'paper'){
-        return 'Paper'
-    } else if(choice.toLowerCase() == 'scissors'){
-        return 'Scissors'
-    }
+function getPlayerChoice() {
+  let choice = prompt("Choose Rock, Paper or Scissors?");
+  if (choice.toLowerCase() == "rock") {
+    return "Rock";
+  } else if (choice.toLowerCase() == "paper") {
+    return "Paper";
+  } else if (choice.toLowerCase() == "scissors") {
+    return "Scissors";
+  }
 }
 
-console.log(getPlayerChoice())
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "Rock" && computerChoice == "Scissors") {
+    return console.log("You Win");
+  } else if (humanChoice == "Rock" && computerChoice == "Paper") {
+    return console.log("You Lose");
+  } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+    return console.log("You Win");
+  } else {
+    return console.log("Its a Tie");
+  }
+}
+
+const humanSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
