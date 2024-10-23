c3f5c3d1d9d9d02eb81a29c6460a1019156aa592
nicolas.patigny <nickim.patigny@gmail.com>
2024-10-23 17:15:39 +0200

    Added function for AI to choose

---
+++ main.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+   let choice = Math.floor(Math.random() * 3);
+   if (choice == 0){
+        return 'Rock'
+   }  else if(choice == 1){
+        return 'Paper'
+   } else if(choice == 2){
+        return 'Scissor'
+   }
+}
+
+
+
+
